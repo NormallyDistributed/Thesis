88177f521e57aacc2f87e3a0784435711bb74377
NormallyDistributed <48487825+NormallyDistributed@users.noreply.github.com>
2020-11-29 20:30:48 +0100

    update

--- thesis_py/main.py
+++ thesis_py/main.py
@@ -9,9 +9,10 @@ import time
 from operator import itemgetter
 from datetime import datetime
 from urllib.error import HTTPError
-from rdflib import URIRef
+from rdflib import URIRef, BNode
 from price_parser import Price
 import datefinder
+
 
 class kg_construction(object):
 
@@ -25,13 +26,14 @@ class kg_construction(object):
                 "external_auditor": "?item wdt:P452 wd:Q23700345. ?item skos:altLabel ?altlabel.",
                 "financial_advisor": "{ {?item wdt:P31 wd:Q613142.} OPTIONAL { ?item skos:altLabel ?altlabel.} } UNION { {?item wdt:P31 wd:Q4830453.} OPTIONAL {?item skos:altLabel ?altlabel.} }",
                 "industry":"{?item wdt:P31 wd:Q8148. ?item skos:altLabel ?altlabel.} UNION {?item wdt:P31 wd:Q268592. ?item skos:altLabel ?altlabel.}",
+                "initial_price_range": "{?item wdt:P31 wd:Q8142. ?item skos:altLabel ?altlabel.}",
                 "investment_bank": "{?item wdt:P31 wd:Q319845. ?item skos:altLabel ?altlabel.} UNION {?item wdt:P31 wd:Q22687. ?item skos:altLabel ?altlabel.} UNION {?item wdt:P31 wd:Q568041. ?item skos:altLabel ?altlabel.} UNION {?item wdt:P31 wd:Q670792. ?item skos:altLabel ?altlabel.} UNION {?item wdt:P31 wd:Q4830453. ?item skos:altLabel ?altlabel.} UNION {?item wdt:P31 wd:Q2111088. ?item skos:altLabel ?altlabel.}",
                 "isin":"?item wdt:P946 ?object. ?item skos:altLabel ?altlabel.",
                 "listing_venue": "?item wdt:P31 wd:Q11691. ?item skos:altLabel ?altlabel.",
                 "non-gaap_measure": "{{?item wdt:P31 wd:Q832161.} OPTIONAL {?item skos:altLabel ?altlabel.}}"
                  }
 
-    def __init__(self): #,dir
+    def __init__(self,dir):
         self.dir = dir
         self.data = None
         self.input = str()
@@ -55,8 +57,6 @@ class kg_construction(object):
                 base = self.data["blobs"][annotation]["annotations"]
                 placeholder.update(base)
         self.data = placeholder
-
-
 
         ## Date extractor
         def xsd_date(item):
@@ -140,22 +140,69 @@ class kg_construction(object):
                     if "million" in item:
                         for number in re.findall(r"([0-9.,]*[0-9]+)", item):
                             if "," in number and "." not in number:
-                                cache.append([number.replace(",", "") + "0" * 3, Price.fromstring(item).currency])
+                                cache.append([item, number.replace(",", "") + "0" * 3, currency_mapper(Price.fromstring(item).currency)])
                             if "," in number and "." in number:
                                 zeros1 = abs(Price.fromstring(number).amount.as_tuple().exponent)
-                                cache.append([number.replace(".", "").replace(",", "") + "0" * (3 - int(zeros1)),
-                                              Price.fromstring(item).currency])
+                                cache.append([item, number.replace(".", "").replace(",", "") + "0" * (3 - int(zeros1)),
+                                             currency_mapper(Price.fromstring(item).currency)])
                             if "." in number and "," not in number:
                                 zeros2 = abs(Price.fromstring(number).amount.as_tuple().exponent)
-                                cache.append([number.replace(".", "") + "0" * (6 - int(zeros2)),
-                                              Price.fromstring(item).currency])
+                                cache.append([item, number.replace(".", "") + "0" * (6 - int(zeros2)),
+                                              currency_mapper(Price.fromstring(item).currency)])
                             if "." not in number and "," not in number:
-                                cache.append([number + "0" * 6, Price.fromstring(item).currency])
+                                cache.append([item, number + "0" * 6, currency_mapper(Price.fromstring(item).currency)])
                         self.data[key] = cache
                     else:
-                        cache.append([str(Price.fromstring(item).amount), Price.fromstring(item).currency, item])
-                        self.data[key] = cache
+                        if "%" in item or "percentage" in item.lower():
+                            for number in re.findall(r"(\d*\.?\d+)%",item.replace(" ","")):
+                                cache.append([item,number,"percentage"])
+                            self.data[key] = cache
+                        else:
+                            cache.append([item,str(Price.fromstring(item).amount), currency_mapper(Price.fromstring(item).currency)])
+                            self.data[key] = cache
+                return self.data[key]
+            except KeyError:
+                pass
 
+        ## Monetary values extractor
+        currencies = {
+            "GBP": "\u00a3",
+            "CNY": "\u00a5",
+            "EUR": "\u20ac",
+            "INR": "\u20B9",
+            "JPY": "\u00a5",
+            "PLN": "z\u0142",
+            "RUB": "\u20BD",
+            "USD": "\u0024",
+        }
+        currency_map = {
+            "GBP": "<https://www.wikidata.org/entity/Q25224>",
+            "CNY": "<https://www.wikidata.org/entity/Q39099>",
+            "EUR": "<https://www.wikidata.org/entity/Q4916>",
+            "INR": "<https://www.wikidata.org/entity/Q80524>",
+            "JPY": "<https://www.wikidata.org/entity/Q8146>",
+            "PLN": "<https://www.wikidata.org/entity/Q123213>",
+            "RUB": "<https://www.wikidata.org/entity/Q41044>",
+            "USD": "<https://www.wikidata.org/entity/Q4917>",
+        }
+
+        def currency_mapper(symbol):
+            try:
+                for key in currencies:
+                    symbol = symbol.replace(currencies[key], key)
+                for key in currency_map:
+                    symbol = symbol.replace(key,currency_map[key])
+                return symbol
+            except AttributeError:
+                pass
+
+        def decimal_values(key):
+            try:
+                for item in self.data[key]:
+                    cache = []
+                    for number in re.findall(r"([0-9.,]*[0-9]+)", item):
+                        cache.append([item, str(Price.fromstring(number).amount),currency_mapper(str(Price.fromstring(item).currency))])
+                        self.data[key] = cache
                 return self.data[key]
             except KeyError:
                 pass
@@ -166,7 +213,7 @@ class kg_construction(object):
                 for item in [re.sub('[^A-Za-z0-9]+', '', item).replace(" ", "").lower() for item in
                              self.data[var]]:
                     if (var.replace("applies","").replace("_","").replace(" ","") in item) or (fuzz.token_set_ratio(var.replace("applies","").replace("_","").replace(" ",""), item) > 85) or (item in ["yes", "true", "1"]):
-                        self.data[var] = "true"
+                        self.data[var] = [str(True)]
             except KeyError:
                 pass
 
@@ -208,13 +255,14 @@ class kg_construction(object):
                               "periods_of_unaudited_financial_statements": xsd_date,
                               "periods_of_pffi": xsd_date,
                               "filing_date": xsd_date,
-                              "underwriting_fees": monetary_values, # need to add percentages
+                              "underwriting_fees": monetary_values,
+                              "underwriters_incentive_fee": monetary_values,
                               "accounting_standards": accounting_categorization,
                               "isin": isin_check,
                               "regulation_s_applies": xsd_boolean,
                               "rule_144a_applies": xsd_boolean,
                               "offering_costs": monetary_values,
-                              "initial_price_range": monetary_values,
+                              "initial_price_range": decimal_values,
                               "expected_gross_proceeds": monetary_values,
                               "expected_net_proceeds": monetary_values
                               }
@@ -222,7 +270,6 @@ class kg_construction(object):
         # apply preprocessing for all keys in preprocessing_dict
         for key in preprocessing_dict.keys():
             preprocessing_dict[key](key)
-
 
     # ## Audit-Report: 4 Types -> {"qualified": "company’s financial records have not been maintained in accordance with GAAP but no misrepresentations are identified, an auditor will issue a qualified opinion",
     # #                            "unqualified": "audit report that is issued when an auditor determines that each of the financial records provided by the small business is free of any misrepresentations+in accordance with GAAP",
@@ -273,14 +320,14 @@ class kg_construction(object):
                 item = {'label': i["itemLabel"]["value"], 'value': i["item"]["value"], "altlabel": i["altlabel"]["value"]}
                 self.results_list.append({self.output: item})
 
-        with open("query_{}.json".format(self.output.replace("/", "")), 'w') as outfile:
+        with open("/Users/mlcb/PycharmProjects/Thesis/thesis_py/queries/query_{}.json".format(self.output.replace("/", "")), 'w') as outfile:
             json.dump(self.results_list, outfile)
 
     def run_query(self):
         for item in self.input_list:
             self.output = item
             self.input = self.input_list.get(item)
-            if os.path.isfile("./query_{}.json".format(self.output.replace("/", ""))) == True:
+            if os.path.isfile("/Users/mlcb/PycharmProjects/Thesis/thesis_py/queries/query_{}.json".format(self.output.replace("/", ""))) == True:
                 kg_construction.fuzzy_matching(self)
             else:
                 try:
@@ -289,16 +336,11 @@ class kg_construction(object):
                     time.sleep(8)
                 kg_construction.fuzzy_matching(self)
 
-        try:
-            print(self.data)
-        except KeyError:
-            pass
-
     def fuzzy_matching(self):
         # link extracted values to wikidata database
 
         # open extracted wikidata entries from wikidata sparql query
-        with open("./query_{}.json".format(self.output.replace("/", "")),"r") as json_file:
+        with open("/Users/mlcb/PycharmProjects/Thesis/thesis_py/queries/query_{}.json".format(self.output.replace("/", "")),"r") as json_file:
             output_file = json_file.read()
             output_file = output_file.replace("'","").replace(',}','}').replace(',]',']').replace('\n','').replace('\t','')
             self.results_list = json.loads(output_file)
@@ -320,7 +362,8 @@ class kg_construction(object):
         #       (i)  if success: use wikidata identifier of closest match
         #       (ii) else: pass
         try:
-            for item in [self.data[self.output][item] for item in range(0,len(self.data[self.output]))]:
+            for item in [self.data[self.output][entry] for entry in range(0,len(self.data[self.output]))]:
+                #cache = [{"label": item}]
                 match = next((x for x in placeholder_list if norm(item) in norm(x[form]) or norm(item) in norm(x["altlabel"])),None)
                 if match != None:
                     if match["value"] not in self.data[self.output]:
@@ -336,12 +379,15 @@ class kg_construction(object):
                                     if closest_match.get("value") not in self.data[self.output]:
                                         self.data[self.output].append(closest_match.get("value"))
 
+
         except KeyError:
             pass
 
     def generate_triples(self):
 
-        prefix_dict = {"rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+        prefix_dict = {
+                       "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+                       "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
                        "wd": "https://wikidata.org/entity/",
                        "omg-spec": "http://www.omg.org/techprocess/ab/SpecificationMetadata/",
                        "omg-lr": "https://www.omg.org/spec/LCC/Languages/LanguageRepresentation/",
@@ -424,14 +470,6 @@ class kg_construction(object):
                         "working_capital_statement": [{"fibo-loan-typ-prod": "WorkingCapitalPurpose"}, {"datatype": "string"}]
                         }
 
-        # def subject():
-        #     if ("isin" in self.data.keys()) and (len(self.data["isin"]) > 1):
-        #         subject = triple_format(self.data["isin"][1])
-        #     else:
-        #         subject = "<{}#{}>".format(URIRef("http://example.org/entity/"),
-        #                                    self.data["issuer_name"][0].replace(" ", ""))
-        #     return subject
-
         def triple_subject():
             subject = triple_format("{}#{}".format(URIRef("http://example.org/entity/"),self.data["issuer_name"][0].replace(" ", "")))
             return subject
@@ -449,31 +487,56 @@ class kg_construction(object):
         def triple_format(item):
             return "<{}>".format(item)
 
-        def xsd_type(type):
-            "^^<https://www.w3.org/2001/XMLSchema#{}>.".format(type)
+        def xsd_type(input):
+            return "^^<https://www.w3.org/2001/XMLSchema#{}>.".format(input)
 
 
         def object(item):
             subject = triple_subject()
             predicate = triple_predicate(item)
-            object = []
             if item in mapping_dict:
-                for element in [self.data[item]]:
-                    for x in element:
-                        if "date" in mapping_dict[item][1].values():
-                            object.append("'{}'{}".format(x,xsd_type(date)))
-                        elif "http" in x:
-                            object.append(triple_format(x))
-                        elif "decimal" in mapping_dict[item][1].values():
-                            object.append("'{}'^^<http://www.w3.org/2001/XMLSchema#decimal>.".format("".join(x)))
-                        elif "duration" in mapping_dict[item][1].values():
-                            object.append("'{}'{}".format(x,xsd_type(duration)))
+                #for element in [" ".join(filter(None,item)) for item in self.data[item]]:
+                for element in [item for item in self.data[item]]:
+                    if "date" in mapping_dict[item][1].values():
+                        object = "'{}'{}".format(element,xsd_type("date"))
+                    elif "http" in element:
+                        object = triple_format(element)
+                    elif "decimal" in mapping_dict[item][1].values():
+                        if "%" == element[0] or element[2] == "percentage":
+                            object = BNode().n3()
+                            print(object, "<http://www.w3.org/1999/02/22-rdf-syntax-ns#value>","{}^^<https://www.w3.org/2001/XMLSchema#decimal>.".format(element[1]))
+                            print(object,"<http://www.w3.org/2000/01/rdf-schema#property>","<https://spec.edmcouncil.org/fibo/ontology/FND/Utilities/Analytics/Percentage>.")
+                            print(object, "<http://www.w3.org/1999/02/22-rdf-syntax-ns#label>","{}.".format(element[0]))
                         else:
-                            object.append("'{}'^^<http://www.w3.org/2001/XMLSchema#string>.".format(x))
-            return subject, predicate
+                            object = BNode().n3()
+                            print(object,"<http://www.w3.org/1999/02/22-rdf-syntax-ns#value>", "{}^^<https://www.w3.org/2001/XMLSchema#decimal>.".format(element[1]))
+                            if element[2] != None:
+                                print(object, "<https://spec.edmcouncil.org/fibo/ontology/FND/Accounting/CurrencyAmount/hasCurrency>","{}.".format(element[2]))
+                            print(object, "<http://www.w3.org/1999/02/22-rdf-syntax-ns#label>","{}.".format(element[0]))
+                    elif "duration" in mapping_dict[item][1].values():
+                        object = "'{}'{}".format(element,xsd_type("duration"))
+                    elif element in [str(True),str(False)]:
+                        object = "'{}'{}".format(element, xsd_type("boolean"))
+                    else:
+                        object = "'{}'^^<http://www.w3.org/2001/XMLSchema#string>.".format(element)
 
-        print(triple_subject(),triple_predicate("listing_venue"), object("listing_venue"))
+                    print(subject,predicate,object)
+            else:
+                #print("UNKNOWN KEY: {}".format(item))
+                pass
+        # try:
+        #     for item in self.data["initial_price_range"]:
+        #         print("0: {}, 1: {}, 2: {}".format(item[0],item[1],item[2]))
+        # except KeyError:
+        #     pass
+        #
+        for instance in self.data:
+            object(instance)
 
+        # try:
+        #     print(self.data["initial_price_range"])
+        # except KeyError:
+        #     pass
 
 
         # # Subject
@@ -506,14 +569,6 @@ class kg_construction(object):
         # sys.stdout.close()
                           #self.data_graph.append([subject,predicate,objekt])
 
-    def metadata(self):
-            pass
-        # dublin_core_namespace = {"dc": "http://purl.org/dc/elements/1.1/"}
-        # for subject in self.data_graph:
-        #     print(self.data_graph)
-        #     #print("subject: {}".format(subject))
-        # pass
-
     def runall(self):
         if __name__ == '__main__':
             Thread(target = self.load_parsed_pdf()).start()
@@ -524,12 +579,21 @@ class kg_construction(object):
 
 
 if __name__ == "__main__":
-    path = "/thesis_py/data/"
+    path = "/Users/mlcb/PycharmProjects/Thesis/thesis_py/data"
     with os.scandir(path) as it:
         for entry in it:
             if entry.name.endswith(".json") and entry.is_file():
                 kg_construction(entry.path).runall()
-                break
+                #break
+
+
+     # def metadata(self):
+     #        pass
+     #    # dublin_core_namespace = {"dc": "http://purl.org/dc/elements/1.1/"}
+     #    # for subject in self.data_graph:
+     #    #     print(self.data_graph)
+     #    #     #print("subject: {}".format(subject))
+     #    # pass
 
 
 
